bec34dfb548d261ee82ad709293f48c6c336167c
Blankkk <mishasokolov2709@gmail.com>
2023-10-03 19:40:44 +0200

    hw1_1

---
+++ Front-End Pro/Homework_1 copy/script.js
@@ -0,0 +1,4 @@
+alert("Hello!");
+let userName = prompt("What is your name?");
+let country = prompt("Where are you from?");
+alert ("Nice to meet you, " +  userName + "!" + " You are from " + country + "!");
