2fc16a6b323437400dc0bd66cff1cffa87280e25
Blankkk <mishasokolov2709@gmail.com>
2023-10-08 10:07:35 +0200

    hw_2_1

---
+++ Front-End Pro/Homework_2/script.js
@@ -0,0 +1,8 @@
+let firstNum = prompt("Enter first number");
+let secondNum = prompt("Enter second number");
+
+console.log("Sum: " + firstNum + "+ " + secondNum + " = " + (Number(firstNum) + Number(secondNum)));
+console.log("Diff: " + firstNum + "- " + secondNum + " = " + (Number(firstNum) - Number(secondNum)));
+console.log("Mult: " + firstNum + "* " + secondNum + " = " + (Number(firstNum) * Number(secondNum)));
+console.log("Div: " + firstNum + "/ " + secondNum + " = " + (Number(firstNum) / Number(secondNum)));
+
