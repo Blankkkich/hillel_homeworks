db3240d02150074503f8c5aebe4dc8cf2aaec3e5
Blankkk <mishasokolov2709@gmail.com>
2023-10-17 11:01:44 +0200

    basic functions

--- Front-End Pro/Homework_5/script.js
+++ Front-End Pro/Homework_5/script.js
@@ -1,7 +1,7 @@
 'use strict'
 
 alert("Welcome to calculator!");
-let action = prompt("What action you want to do?\n" 
+const action = prompt("What action you want to do?\n" 
     + "\nAdd" + "\nDiff" + "\nMult" + "\nDiv"
     + "\nSqrt" + "\nSin" + "\nCos");
 
@@ -9,27 +9,50 @@ if (action === null || NaN || undefined) {
     alert("Goodbye, see you later.");
 } else {
     if (action === "Add" || action === "Diff" || action === "Mult" || action === "Div" ||action === "Cos" || action === "Sin"){
-        let num1 = Number(prompt("Enter first number"));
-        let num2 = Number(prompt("Enter second number"));
+        const num1 = Number(prompt("Enter first number"));
+        const num2 = Number(prompt("Enter second number"));
         if (num1 && num2 === NaN || undefined || null) {
             alert("Invalid number. Good bye!")
         }
         if(action === "Add"){
-            let result = num1 + num2;
+            const result = num1 + num2;
             alert("Sum of " + num1 + " and " + num2 + " is " +result);
         }else if(action === "Diff"){
-            let result = num1 - num2;
+            const result = num1 - num2;
             alert("Substraction of " + num1 + " and " + num2 + " is " +result);
         }else if(action === "Mult"){
-            let result = num1 * num2;
+            const result = num1 * num2;
             alert("Product of " + num1 + " and " + num2 + " is " +result);
         }else if(action === "Div"){
             if (num2 !== 0) {
-                let result = num1 / num2;
+                const result = num1 / num2;
                 alert("Quotient of " + num1 + " and " + num2 + " is " +result);
             } else {
                 alert('Error: Division by zero is not allowed. Good bye!');
             }
         }
+    } else if (action ==="Sqrt") {
+        let num = Number(prompt("Enter a number"));
+        if (num >= 0) {
+            const numSqrt = Math.sqrt(num);
+            alert("Square root of " + num + " is " + numSqrt)
+        } else {
+            alert("Error: Square root of a negative number if high math dumbass. Good bye!")
+        }
+    } else if (action === "Sin" || action === "Cos") {
+        const angle = Number(prompt("Enter an angle in degrees"));
+        if(!isNaN(angle)){
+            if(action === "Sin"){
+                const sin = Math.sin(angle * (Math.PI / 180));
+                alert("Sine of " + angle + " degrees is " + sin);
+            } else if (action === "Cos") {
+                var cos = Math.cos(angle * (Math.PI / 180));
+                alert("Cosine of " + angle + " degrees is " + cos);
+            }
+        } else {
+            alert("Error: Invalid input for angle.");
+        }
+    } else {
+        alert("Error: Invalid action. Please select one of the specified options.");
     }
 }
