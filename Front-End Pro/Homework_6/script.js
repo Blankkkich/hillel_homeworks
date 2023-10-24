2db3f4dcf97fbaa1297dee9e5530fabf8ded4172
Blankkk <mishasokolov2709@gmail.com>
2023-10-24 18:41:53 +0200

    small start

---
+++ Front-End Pro/Homework_6/script.js
@@ -0,0 +1,22 @@
+alert("Welcome to calculator!");
+
+let start = true;
+
+    do {
+    const operation = prompt("What action you want to do? Add, Diff, Mult, Div, Sqrt, Sin, Cos", "Add");
+
+    if (operation) {
+    case "add":
+    case "diff":
+    case "mult":
+    case "div": {
+        let
+        let
+
+            do {
+
+            } while (!Number.isInteger(firstNum))
+        }
+
+    }
+    } while ()
