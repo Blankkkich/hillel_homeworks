aa6ce37daf7068e2c9c8926da8e648d9fff27f16
Blankkk <mishasokolov2709@gmail.com>
2023-11-04 14:15:23 +0100

    closures

---
+++ Front-End Pro/Closures/script.js
@@ -0,0 +1,14 @@
+function createSum() {
+    let total = 0;
+
+    return function (num) {
+        total += num;
+        return total;
+    };
+}
+
+const sum = createSum();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
